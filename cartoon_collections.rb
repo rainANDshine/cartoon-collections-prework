29252e0191f9ac2629790588fae7095cea128537
Shun Yao <ysmiracle@hotmail.com>
2018-03-28 07:59:25 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,9 @@ def summon_captain_planet(calls)
 end
 
 def long_planeteer_calls(calls)
-  # Your code here
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
