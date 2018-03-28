5018f8c546307bfc4659a250eeb0b99702c2c1be
Shun Yao <ysmiracle@hotmail.com>
2018-03-28 08:01:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
+  snacks.include?
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
